016ce922b67a3deafc640a4a7dc6fc05c6d4f36b
kerlomz <kerlomz@gmail.com>
2020-06-19 15:10:10 +0800

    解决gif融合处理equalizeHist报错

--- tools/gif_frames.py
+++ tools/gif_frames.py
@@ -46,7 +46,8 @@ def blend_arr(img_arr):
     all_slice = img_arr[0]
     for im_slice in img_arr[1:]:
         all_slice = cv2.addWeighted(all_slice, 0.5, im_slice, 0.5, 0)
-    all_slice = cv2.equalizeHist(all_slice)
+    # print(all_slice)
+    # all_slice = cv2.equalizeHist(all_slice)
     return all_slice
 
 
@@ -55,6 +56,8 @@ def blend_frame(image_obj, need_frame=None):
         need_frame = [-1]
     img_arr = split_frames(image_obj, need_frame)
     img_arr = blend_arr(img_arr)
+    img_arr = cv2.cvtColor(img_arr, cv2.COLOR_RGB2GRAY)
+    img_arr = cv2.equalizeHist(img_arr)
     return img_arr
 
 
